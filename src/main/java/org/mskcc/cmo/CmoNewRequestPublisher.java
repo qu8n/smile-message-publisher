00677c656dd5fee572795f29c3fd86ac468b42c7
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2021-01-12 17:03:45 -0500

    Update publisher: publish IGO new requests to CMO MetaDB
    Request IDs can be passed as command line argument for calling the LIMS Rest service
    
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

---
+++ src/main/java/org/mskcc/cmo/CmoNewRequestPublisher.java
@@ -0,0 +1,201 @@
+package org.mskcc.cmo;
+
+import com.google.gson.Gson;
+import java.security.cert.X509Certificate;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import javax.net.ssl.HostnameVerifier;
+import javax.net.ssl.SSLContext;
+import org.apache.commons.lang3.StringUtils;
+import org.apache.commons.logging.Log;
+import org.apache.commons.logging.LogFactory;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.conn.ssl.SSLContexts;
+import org.apache.http.conn.ssl.TrustStrategy;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+import org.mskcc.cmo.messaging.Gateway;
+import org.mskcc.cmo.shared.SampleManifest;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.http.HttpEntity;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.web.client.RestTemplate;
+
+/**
+ *
+ * @author DivyaMadala
+ */
+@SpringBootApplication(scanBasePackages = "org.mskcc.cmo.messaging")
+public class CmoNewRequestPublisher implements CommandLineRunner {
+
+    @Autowired
+    private Gateway messagingGateway;
+
+    @Value("${igo.new_request_topic}")
+    private String IGO_NEW_REQUEST_TOPIC;
+
+    @Value("${lims.base_url}")
+    private String limsBaseUrl;
+
+    @Value("${lims.username}")
+    private String limsUsername;
+
+    @Value("${lims.password}")
+    private String limsPassword;
+
+    @Value("${lims.request_samples_endpoint}")
+    private String limsRequestSamplesEndpoint;
+
+    @Value("${lims.sample_manifest_endpoint}")
+    private String limsSampleManifestEndpoint;
+
+    private final Log log = LogFactory.getLog(CmoNewRequestPublisher.class);
+
+    public static void main(String[] args) throws Exception {
+        SpringApplication.run(CmoNewRequestPublisher.class, args);
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        String requestIds = args[0];
+        messagingGateway.connect();
+        Gson gson = new Gson();
+        try {
+            for (String r : requestIds.split(",")) {
+                Map<String, Object> request = fetchRequestAndSamplesFromLims(r);
+                String requestJson = gson.toJson(request);
+                log.info("\nPublishing IGO new request to MetaDB:\n\n"
+                        + requestJson + "\n\n on topic: " + IGO_NEW_REQUEST_TOPIC);
+                messagingGateway.publish(IGO_NEW_REQUEST_TOPIC, requestJson);
+            }
+        } catch (Exception e) {
+            log.error("Error encountered during attempt to process request ids - exiting...");
+            throw new RuntimeException(e);
+        }
+        log.info("Finished processing requests, exiting...");
+        System.exit(0);
+    }
+
+    /**
+     * Returns the sample manifest list for all samples given a request id.
+     * @param requestId
+     * @return Map
+     * @throws Exception
+     */
+    private Map<String, Object> fetchRequestAndSamplesFromLims(String requestId) throws Exception {
+        // call lims api for given request id...
+        // LIMS endpoint: /publishIgoRequestToMetaDb,
+        //      params: requestId (String), projectId (String, optional)
+        // response = response from LIMS /publishIgoRequestToMetaDb endpoint
+        // key-value pairs:
+        // requestId: string
+        // projectId: string
+        // sampleManifestList: List<SampleManifest>
+
+        // fetch request samples from getRequestSamples endpoint
+        // then fetch sample manifest for each sample from getSampleManifest endpoint
+        Map<String, Object> limsResponse = getLimsRequestSamples(requestId);
+        List<SampleManifest> sampleManifestList = getSampleManifestList(limsResponse);
+
+        Map<String, Object> toReturn = new HashMap<>();
+        toReturn.put("requestId", requestId);
+        toReturn.put("sampleManifestList", sampleManifestList);
+        // we dont know the project id from getRequestSamples
+        // this cannot be added to the returned map yet
+        //toReturn.put("projectId", response.get("projectId"));
+        return toReturn;
+    }
+
+    /**
+     * Calls LimsRest and returns list of SampleManifest instances for a request id.
+     * @param response
+     * @return List
+     * @throws Exception
+     */
+    private List<SampleManifest> getSampleManifestList(Map<String, Object> response) throws Exception {
+        // get sample ids and compile into param to pass to getSampleManifest endpoint
+        List<String> sampleIds = getSampleIdsFromRequestResponse(response);
+        String sampleIdsParam = StringUtils.join(sampleIds, "&igoSampleId=");
+        String manifestUrl = limsBaseUrl + limsSampleManifestEndpoint + sampleIdsParam;
+        log.debug("Sending request for sample manifest with url:" + manifestUrl);
+
+        RestTemplate restTemplate = getRestTemplate();
+        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = getRequestEntity();
+        ResponseEntity<SampleManifest[]> responseEntity = restTemplate.exchange(manifestUrl,
+                HttpMethod.GET, requestEntity, SampleManifest[].class);
+        return Arrays.asList(responseEntity.getBody());
+    }
+
+    /**
+     * Calls LimsRest and returns the list of samples for a given request id.
+     * @param requestId
+     * @return Map
+     * @throws Exception
+     */
+    private Map<String, Object> getLimsRequestSamples(String requestId) throws Exception {
+        Gson gson = new Gson();
+        String requestUrl = limsBaseUrl + limsRequestSamplesEndpoint + requestId;
+        RestTemplate restTemplate = getRestTemplate();
+        HttpEntity<LinkedMultiValueMap<String, Object>> requestEntity = getRequestEntity();
+        ResponseEntity responseEntity = restTemplate.exchange(requestUrl,
+                HttpMethod.GET, requestEntity, Object.class);
+        Map<String, Object> response = gson.fromJson(gson.toJson(responseEntity.getBody()), Map.class);
+        log.debug("Response from LIMS:\n" + gson.toJson(response));
+        return response;
+    }
+
+    /**
+     * Extracts list of sample ids as strings from the LIMS response.
+     * @param response
+     * @return
+     */
+    private List<String> getSampleIdsFromRequestResponse(Map<String, Object> response) {
+        Gson gson = new Gson();
+        String samplesListJson = gson.toJson(response.get("samples"));
+        List<Map> samplesListMap = gson.fromJson(samplesListJson, List.class);
+        List<String> sampleIds = new ArrayList<>();
+        for (Map m : samplesListMap) {
+            sampleIds.add((String) m.get("igoSampleId"));
+        }
+        return sampleIds;
+    }
+
+    /**
+     * Returns rest template that by passes SSL cert check.
+     * @return RestTemplate
+     * @throws Exception
+     */
+    private RestTemplate getRestTemplate() throws Exception {
+        TrustStrategy acceptingTrustStrategy = (X509Certificate[] chain, String authType) -> true;
+        HostnameVerifier hostnameVerifier = (s, sslSession) -> true;
+        SSLContext sslContext = SSLContexts.custom().loadTrustMaterial(null, acceptingTrustStrategy).build();
+        SSLConnectionSocketFactory csf = new SSLConnectionSocketFactory(sslContext, hostnameVerifier);
+        CloseableHttpClient httpClient = HttpClients.custom().setSSLSocketFactory(csf).build();
+        HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
+        requestFactory.setHttpClient(httpClient);
+        return new RestTemplate(requestFactory);
+    }
+
+    /**
+     * Returns request entity.
+     * @return HttpEntity
+     */
+    private HttpEntity getRequestEntity() {
+        HttpHeaders headers = new HttpHeaders();
+        headers.setContentType(MediaType.APPLICATION_JSON);
+        headers.setBasicAuth(limsUsername, limsPassword);
+        return new HttpEntity<Object>(headers);
+    }
+}
