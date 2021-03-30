fde591146522b15e59e5f85f3b8add39655f50e4
Angelica Ochoa <15623749+ao508@users.noreply.github.com>
2021-03-30 12:22:14 -0400

    Update spring boot version and logging
    Signed-off-by: Angelica Ochoa <15623749+ao508@users.noreply.github.com>

--- src/main/java/org/mskcc/cmo/publisher/pipeline/config/BatchConfiguration.java
+++ src/main/java/org/mskcc/cmo/publisher/pipeline/config/BatchConfiguration.java
@@ -1,7 +1,9 @@
 package org.mskcc.cmo.publisher.pipeline.config;
 
+import java.net.MalformedURLException;
 import java.util.Map;
 import java.util.concurrent.Future;
+import javax.sql.DataSource;
 import org.mskcc.cmo.messaging.Gateway;
 import org.mskcc.cmo.publisher.pipeline.MetaDbFilePublisherListener;
 import org.mskcc.cmo.publisher.pipeline.MetaDbFilePublisherReader;
@@ -17,19 +19,29 @@ import org.springframework.batch.core.configuration.annotation.EnableBatchProces
 import org.springframework.batch.core.configuration.annotation.JobBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepBuilderFactory;
 import org.springframework.batch.core.configuration.annotation.StepScope;
+import org.springframework.batch.core.launch.JobLauncher;
+import org.springframework.batch.core.launch.support.SimpleJobLauncher;
+import org.springframework.batch.core.repository.JobRepository;
+import org.springframework.batch.core.repository.support.JobRepositoryFactoryBean;
 import org.springframework.batch.integration.async.AsyncItemProcessor;
 import org.springframework.batch.integration.async.AsyncItemWriter;
 import org.springframework.batch.item.ItemProcessor;
 import org.springframework.batch.item.ItemStreamReader;
 import org.springframework.batch.item.ItemStreamWriter;
 import org.springframework.batch.item.ItemWriter;
+import org.springframework.batch.support.transaction.ResourcelessTransactionManager;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.Resource;
+import org.springframework.jdbc.datasource.DriverManagerDataSource;
+import org.springframework.jdbc.datasource.init.DataSourceInitializer;
+import org.springframework.jdbc.datasource.init.ResourceDatabasePopulator;
 import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
+import org.springframework.transaction.PlatformTransactionManager;
 
 /**
  *
@@ -38,7 +50,7 @@ import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 @Configuration
 @EnableBatchProcessing
 @EnableAsync
-@ComponentScan(basePackages = "org.mskcc.cmo.messaging")
+@ComponentScan(basePackages = {"org.mskcc.cmo.messaging", "org.mskcc.cmo.common.*"})
 public class BatchConfiguration {
 
     public static final String LIMS_REQUEST_PUBLISHER_JOB = "limsRequestPublisherJob";
@@ -244,5 +256,76 @@ public class BatchConfiguration {
     @Bean
     public StepExecutionListener limsRequestListener() {
         return new LimsRequestListener();
+    }
+
+    // general spring batch configuration
+    @Value("org/springframework/batch/core/schema-drop-sqlite.sql")
+    private Resource dropRepositoryTables;
+
+    @Value("org/springframework/batch/core/schema-sqlite.sql")
+    private Resource dataRepositorySchema;
+
+    /**
+     * Spring Batch datasource.
+     * @return DataSource
+     */
+    @Bean
+    public DataSource dataSource() {
+        DriverManagerDataSource dataSource = new DriverManagerDataSource();
+        dataSource.setDriverClassName("org.sqlite.JDBC");
+        dataSource.setUrl("jdbc:sqlite:repository.sqlite");
+        return dataSource;
+    }
+
+    /**
+     * Spring Batch datasource initializer.
+     * @param dataSource
+     * @return DataSourceInitializer
+     * @throws MalformedURLException
+     */
+    @Bean
+    public DataSourceInitializer dataSourceInitializer(DataSource dataSource) throws MalformedURLException {
+        ResourceDatabasePopulator databasePopulator = new ResourceDatabasePopulator();
+        databasePopulator.addScript(dropRepositoryTables);
+        databasePopulator.addScript(dataRepositorySchema);
+        databasePopulator.setIgnoreFailedDrops(true);
+
+        DataSourceInitializer initializer = new DataSourceInitializer();
+        initializer.setDataSource(dataSource);
+        initializer.setDatabasePopulator(databasePopulator);
+        return initializer;
+    }
+
+    /**
+     * Spring Batch job repository.
+     * @return JobRepository
+     * @throws Exception
+     */
+    private JobRepository getJobRepository() throws Exception {
+        JobRepositoryFactoryBean factory = new JobRepositoryFactoryBean();
+        factory.setDataSource(dataSource());
+        factory.setTransactionManager(getTransactionManager());
+        factory.afterPropertiesSet();
+        return (JobRepository) factory.getObject();
+    }
+
+    /**
+     * Spring Batch transaction manager.
+     * @return PlatformTransactionManager
+     */
+    private PlatformTransactionManager getTransactionManager() {
+        return new ResourcelessTransactionManager();
+    }
+
+    /**
+     * Spring Batch job launcher.
+     * @return JobLauncher
+     * @throws Exception
+     */
+    public JobLauncher getJobLauncher() throws Exception {
+        SimpleJobLauncher jobLauncher = new SimpleJobLauncher();
+        jobLauncher.setJobRepository(getJobRepository());
+        jobLauncher.afterPropertiesSet();
+        return jobLauncher;
     }
 }
